e65f3f72ce45eaa688ca2f301c49b42121af7c92
FedericoRubino <frubino@ciginsurance.com>
2024-07-03 11:40:11 -0700

    basic functionality to work.

--- app/src/main/java/com/example/wordlehelper/MainActivity.java
+++ app/src/main/java/com/example/wordlehelper/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.wordlehelper;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,22 +45,125 @@ public class MainActivity extends AppCompatActivity {
         greyLettersET = findViewById(R.id.grey);
         guessWordET = findViewById(R.id.guess);
 
+        outputLV = findViewById(R.id.outputList);
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void doWork() {
+        List<String> words;
+
+        try {
+            words = loadWords("PossibleAnswers.txt");
+        } catch (IOException e) {
+            System.out.println("Error loading word files: " + e.getMessage());
+            return;
+        }
+
+        List<String> possibleWords = getPossibleWords(words, yellowS, greyS, guessS);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                possibleWords
+        );
+
+        outputLV.setAdapter(arrayAdapter);
+
     }
 
+    private List<String> loadWords(String filePath) throws IOException {
+        List<String> words = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filePath)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim());
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+
+    private static boolean alreadyUsed(String word, List<String> pastWords) {
+        for (String pastWord : pastWords) {
+            if (word.equalsIgnoreCase(pastWord)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static List<String> getPossibleWords(List<String> words, String yellowLetters, String greyLetters,
+                                                 String guess) {
+        List<String> possibleWords = new ArrayList<>();
+        List<String> pastAnswers = new ArrayList<>();
+        Map<Character, Integer> yellowCount = new HashMap<>();
+
+        for (char c : yellowLetters.toCharArray()) {
+            yellowCount.put(c, yellowCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (String word : words) {
+            Map<Character, Integer> wordCount = new HashMap<>();
+            int yellowMatchCount = 0;
+            boolean greyInWord = false;
+
+            for (char c : word.toCharArray()) {
+                wordCount.put(c, wordCount.getOrDefault(c, 0) + 1);
+            }
+
+            for (char c : word.toCharArray()) {
+                if (wordCount.getOrDefault(c, 0) <= yellowCount.getOrDefault(c, 0)) {
+                    yellowMatchCount++;
+                }
+            }
+
+            for (int i = 0; i < 5; i++) {
+                if (greyLetters.contains(String.valueOf(word.charAt(i))) && word.charAt(i) != guess.charAt(i)) {
+                    greyInWord = true;
+                    break;
+                }
+            }
+
+            if (yellowMatchCount >= yellowLetters.length() && !greyInWord) {
+                for (int i = 0; i < 5; i++) {
+                    if (word.charAt(i) != guess.charAt(i) && guess.charAt(i) != '0') {
+                        break;
+                    } else if (i == 4) {
+                        if (alreadyUsed(word, pastAnswers)) {
+                            pastAnswers.add(word);
+                        } else {
+                            possibleWords.add(word);
+                        }
+                    }
+                }
+            }
+        }
+        return possibleWords;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void onButtonPressWordle(View view) {
         if(!yelowLettersET.getText().toString().equals("")){
             yellowS = yelowLettersET.getText().toString();
+        } else {
+            yellowS = "";
         }
         if(!greyLettersET.getText().toString().equals("")){
             greyS = greyLettersET.getText().toString();
+        } else {
+            greyS = "";
         }
         if(!guessWordET.getText().toString().equals("") && guessWordET.getText().toString().length() == 5){
             guessS = guessWordET.getText().toString();
         } else {
+            guessS = "00000";
             Toast.makeText(this,"Guess needs to be 5 letters long", Toast.LENGTH_LONG).show();
             return;
         }
